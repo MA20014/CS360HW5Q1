14e24b1a0318abe7856ad1c846f53579125c30e9
MA20014 <145225394+MA20014@users.noreply.github.com>
2024-04-04 23:21:46 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,93 +1,169 @@
 #include <iostream>
 #include <string>
-
+#include <iomanip>
 using namespace std;
 
 class Package {
-protected:
-    string senderName;
-    string senderAddress;
-    string senderCity;
-    string senderState;
-    string senderZip;
-    string recipientName;
-    string recipientAddress;
-    string recipientCity;
-    string recipientState;
-    string recipientZip;
+private:
+    string s_name;
+    string s_address;
+    string s_city;
+    string s_state;
+    string s_ZIPcode;
+    string r_name;
+    string r_address;
+    string r_city;
+    string r_state;
+    string r_ZIPcode;
     double weight;
-    double costPerOunce;
+    double costperounce;
 
 public:
-    Package(const string& senderName, const string& senderAddress, const string& senderCity,
-            const string& senderState, const string& senderZip, const string& recipientName,
-            const string& recipientAddress, const string& recipientCity, const string& recipientState,
-            const string& recipientZip, double weight, double costPerOunce)
-        : senderName(senderName), senderAddress(senderAddress), senderCity(senderCity),
-          senderState(senderState), senderZip(senderZip), recipientName(recipientName),
-          recipientAddress(recipientAddress), recipientCity(recipientCity), recipientState(recipientState),
-          recipientZip(recipientZip), weight(max(weight, 0.0)), costPerOunce(max(costPerOunce, 0.0)) {}
-
-    virtual double calculateCost() const {
-        return weight * costPerOunce;
+    Package(string s_n, string s_addr, string s_cy, string s_st, string s_ZIP,
+            string r_n, string r_addr, string r_cy, string r_st, string r_ZIP,
+            double w, double cost) {
+        s_name = s_n;
+        s_address = s_addr;
+        s_city = s_cy;
+        s_state = s_st;
+        s_ZIPcode = s_ZIP;
+        r_name = r_n;
+        r_address = r_addr;
+        r_city = r_cy;
+        r_state = r_st;
+        r_ZIPcode = r_ZIP;
+        if (w > 0.0 && cost > 0.0) {
+            weight = w;
+            costperounce = cost;
+        } else {
+            weight = 0.0;
+            costperounce = 0.0;
+        }
     }
+
+    string getsender_name();
+    string getsender_address();
+    string getSendCity();
+    string getsender_state();
+    string getsender_ZIP();
+    string getrecipient_name();
+    string getrecipient_address();
+    string getrecipient_city();
+    string getrecipient_state();
+    string getrecipient_ZIP();
+    double getweight();
+    double getcostperounce();
+    double calculateCost();
 };
+
+string Package::getsender_name() { return s_name; }
+string Package::getsender_address() { return s_address; }
+string Package::getSendCity() { return s_city; }
+string Package::getsender_state() { return s_state; }
+string Package::getsender_ZIP() { return s_ZIPcode; }
+string Package::getrecipient_name() { return r_name; }
+string Package::getrecipient_address() { return r_address; }
+string Package::getrecipient_city() { return r_city; }
+string Package::getrecipient_state() { return r_state; }
+string Package::getrecipient_ZIP() { return r_ZIPcode; }
+double Package::getweight() { return weight; }
+double Package::getcostperounce() { return costperounce; }
+
+double Package::calculateCost() {
+    double result;
+    result = weight * costperounce;
+    return result;
+}
 
 class TwoDayPackage : public Package {
 private:
-    double flatFee;
-
+    double flat_fee;
 public:
-    TwoDayPackage(const string& senderName, const string& senderAddress, const string& senderCity,
-                  const string& senderState, const string& senderZip, const string& recipientName,
-                  const string& recipientAddress, const string& recipientCity, const string& recipientState,
-                  const string& recipientZip, double weight, double costPerOunce, double flatFee)
-        : Package(senderName, senderAddress, senderCity, senderState, senderZip, recipientName,
-                  recipientAddress, recipientCity, recipientState, recipientZip, weight, costPerOunce),
-          flatFee(flatFee) {}
-
-    double calculateCost() const override {
-        return Package::calculateCost() + flatFee;
-    }
+    TwoDayPackage(string s_n, string s_addr, string s_cy, string s_st, string s_ZIP,
+                  string r_n, string r_addr, string r_cy, string r_st, string r_ZIP,
+                  double w, double cost, double f_fee) : Package(s_n, s_addr, s_cy, s_st, s_ZIP, r_n,
+                  r_addr, r_cy, r_st, r_ZIP, w, cost), flat_fee(f_fee) {}
+    double calculateCost();
 };
+
+double TwoDayPackage::calculateCost() {
+    double result;
+    result = Package::calculateCost() + flat_fee;
+    return result;
+}
 
 class OvernightPackage : public Package {
 private:
-    double additionalFeePerOunce;
-
+    double additional_fee;
 public:
-    OvernightPackage(const string& senderName, const string& senderAddress, const string& senderCity,
-                     const string& senderState, const string& senderZip, const string& recipientName,
-                     const string& recipientAddress, const string& recipientCity, const string& recipientState,
-                     const string& recipientZip, double weight, double costPerOunce, double additionalFeePerOunce)
-        : Package(senderName, senderAddress, senderCity, senderState, senderZip, recipientName,
-                  recipientAddress, recipientCity, recipientState, recipientZip, weight, costPerOunce),
-          additionalFeePerOunce(additionalFeePerOunce) {}
-
-    double calculateCost() const override {
-        return Package::calculateCost() + (weight * additionalFeePerOunce);
-    }
+    OvernightPackage(string s_n, string s_addr, string s_cy, string s_st, string s_ZIP,
+                      string r_n, string r_addr, string r_cy, string r_st, string r_ZIP,
+                      double w, double cost, double addl_fee) : Package(s_n, s_addr, s_cy, s_st, s_ZIP, r_n,
+                      r_addr, r_cy, r_st, r_ZIP, w, cost), additional_fee(addl_fee) {}
+    double calculateCost();
 };
 
+double OvernightPackage::calculateCost() {
+    double result;
+    result = (getcostperounce() + additional_fee) * getweight();
+    return result;
+}
+
 int main() {
-    // Testing the classes
-    Package package("Sender Name", "Sender Address", "Sender City", "Sender State", "Sender Zip",
-                    "Recipient Name", "Recipient Address", "Recipient City", "Recipient State", "Recipient Zip",
-                    10, 0.5);
+    string sname, saddr, scity, sstate, szip, rname, raddr, rcity, rstate, rzip;
+    double w;
+    cout << "Enter the sender Name: ";
+    getline(cin, sname);
+    cout << "Enter the sender Address: ";
+    getline(cin, saddr);
+    cout << "Enter the sender's city: ";
+    getline(cin, scity);
+    cout << "Enter the sender's state: ";
+    getline(cin, sstate);
+    cout << "Enter the sender ZIP code: ";
+    getline(cin, szip);
+    cout << "Enter the package weight: ";
+    cin >> w;
+    cin.ignore(); // to clear the newline character from the input buffer
+    cout << "Enter the Receiver's Name: ";
+    getline(cin, rname);
+    cout << "Enter the Receiver's Address: ";
+    getline(cin, raddr);
+    cout << "Enter the Receiver's city: ";
+    getline(cin, rcity);
+    cout << "Enter the Receiver's state: ";
+    getline(cin, rstate);
+    cout << "Enter the Receiver's ZIP code: ";
+    getline(cin, rzip);
 
-    cout << "Package Cost: $" << package.calculateCost() << endl;
+    TwoDayPackage item2 = TwoDayPackage(sname, saddr, scity, sstate, szip, rname, raddr, rcity, rstate, rzip, w, 1.5, 1.10);
+    OvernightPackage item1 = OvernightPackage(sname, saddr, scity, sstate, szip, rname, raddr, rcity, rstate, rzip, w, 1.5, 2.20);
 
-    TwoDayPackage twoDayPackage("Sender Name", "Sender Address", "Sender City", "Sender State", "Sender Zip",
-                                "Recipient Name", "Recipient Address", "Recipient City", "Recipient State", "Recipient Zip",
-                                10, 0.5, 10);
+    cout << fixed << setprecision(2);
+    cout << endl;
+    cout << "Overnight Delivery\n";
+    cout << endl;
+    cout << "Sender " << item1.getsender_name() << "\n";
+    cout << " " << item1.getsender_address() << "\n";
+    cout << " " << item1.getSendCity() << " " << item1.getsender_state() << " " << item1.getsender_ZIP() << "\n";
+    cout << "\n";
+    cout << "Recipient " << item1.getrecipient_name() << "\n";
+    cout << " " << item1.getsender_address() << "\n";
+    cout << " " << item1.getrecipient_city() << " " << item1.getrecipient_state() << " " << item1.getrecipient_ZIP() << "\n";
+    cout << "Cost $ " << item1.calculateCost() << "\n";
 
-    cout << "Two Day Package Cost: $" << twoDayPackage.calculateCost() << endl;
-
-    OvernightPackage overnightPackage("Sender Name", "Sender Address", "Sender City", "Sender State", "Sender Zip",
-                                      "Recipient Name", "Recipient Address", "Recipient City", "Recipient State", "Recipient Zip",
-                                      10, 0.5, 1);
-
-    cout << "Overnight Package Cost: $" << overnightPackage.calculateCost() << endl;
+    cout << "\n\n";
+    cout << endl;
+    cout << "2 Day Delivery\n";
+    cout << "Sender " << item2.getsender_name() << "\n";
+    cout << " " << item2.getsender_address() << "\n";
+    cout << " " << item2.getSendCity() << " " << item2.getsender_state() << " " << item2.getsender_ZIP() << "\n";
+    cout << "\n";
+    cout << "Recipient " << item2.getrecipient_name() << "\n";
+    cout << " " << item2.getsender_address() << "\n";
+    cout << " " << item2.getrecipient_city() << " " << item2.getrecipient_state() << " " << item2.getrecipient_ZIP() << "\n";
+    cout << "Cost $ " << item2.calculateCost() << "\n";
+    cout << endl;
 
     return 0;
 }
